bea5de9672efecc574011df4a432bf2e9b5066d4
Mubtasim Ihtisham Ove <mubtasim26@gmail.com>
2026-03-04 00:43:35 +0600

    edit price name

--- src/app/(admin)/products/create/_components/CreateProduct.tsx
+++ src/app/(admin)/products/create/_components/CreateProduct.tsx
@@ -362,9 +362,9 @@ console.log(watch())
           {/* ---------------- Price / Stock ---------------- */}
           <div className="grid grid-cols-1 gap-6 md:grid-cols-4">
             {[
-              { name: "price", label: "Price *" },
+              { name: "price", label: "Product Price *" },
               { name: "previousPrice", label: "Previous Price" },
-              { name: "extraPrice", label: "Extra Price" },
+              { name: "extraPrice", label: "Handling Charge" },
               { name: "stock", label: "Stock" },
             ].map(({ name, label }) => (
               <div key={name}>
